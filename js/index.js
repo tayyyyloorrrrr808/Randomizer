2c73cac59b9fe44c380728d429b74a635b556304
Taylor <tmoody21@punahou.edu>
2016-06-29 14:24:48 -1000

    Artists Randomizer
    random artists

---
+++ js/index.js
@@ -0,0 +1,37 @@
+var Artists =  
+["Justin Bieber",
+ "Konye West",
+  "Beyonce",
+   "Rihanna",
+   "Selena Gomez",
+    "Ariana Grande",
+     "PartyNextDoor", "Chris Brown", 
+     "Demi Lovato", "Fetty Wap", 
+     "Tove Lo",
+     "Charlie Puth", 
+     "Twenty One pilots",
+     "Meghan Trainor",
+     "Future",
+     "Kent Jones",
+     "Ruth B",
+     "Daya",
+     "G-Eazy",
+     "Alessia Cara",
+     "Ty Dolla $ign",
+     "DJ Khaled",
+     "Travi$ Scott",
+     "Tory Lanez",
+     "Post Malone",
+     "Jason Derulo",
+     "Kid Ink",
+     "R. City"];
+
+function pickArtists(){
+	// Sets avariablle called toppingNumber that will have a random Whole Number
+	//Based on the length of the Artists array
+	var artistsNumber = Math.floor(Math.random()*Artists.length);
+	// Sets avariablle called toppingNumber2 that will have a random Whole Number
+	//Based on the length of the Artists array
+	var artistsNumber2 = Math.floor(Math.random()*Artists.length);
+	document.getElementById('mixArtists').innerHTML = "<h1>"+Artists[artistsNumber]+" And "+Artists[artistsNumber2]+"</h1>";
+}
